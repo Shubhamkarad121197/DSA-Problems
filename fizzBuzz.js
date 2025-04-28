68c46c1a081bc38b18ae4fc13c965817d67bda52
Shubham karad <90271066+Shubhamkarad121197@users.noreply.github.com>
2025-04-28 17:52:04 +0000

    FizzBuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,20 @@
+function fizBuzzFunc(n){
+    let result=[];
+    for(i=1;i<=n;i++){
+        if(i%3==0 && i%5==0){
+            result.push("FizzBuzz")
+        }
+        else if(i%3==0 ){
+            result.push("Fizz")
+        }
+        else if(i%5==0){
+            result.push('Buzz')
+        }
+        else{
+            result.push(i)
+        }
+    }
+    return result;
+}
+
+console.log(fizBuzzFunc(30))
