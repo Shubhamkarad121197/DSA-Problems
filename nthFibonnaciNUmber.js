a03e018aa050793a37aca861d209e38bcddd58ab
Shubham karad <90271066+Shubhamkarad121197@users.noreply.github.com>
2025-05-22 17:25:17 +0000

    fibonnaci

--- nthFibonnaciNUmber.js
+++ nthFibonnaciNUmber.js
@@ -9,3 +9,16 @@ function nthFibonnaci(n){
 }
 
 console.log(nthFibonnaci(10))
+
+
+
+function generateFibonacci(n) {
+    let fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib;
+}
+
+let result = generateFibonacci(50);
+console.log(result);
